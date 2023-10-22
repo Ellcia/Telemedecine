d5dcfb1c963a5c8e8d6fd68fbfb2ce3b2147fb8a
Ellcia <78089608+Ellcia@users.noreply.github.com>
2023-10-23 00:55:31 +0200

    fdnhdgq

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas as pd
-import requests
 from getLib import getJson
+from post_processing import post_processing
 
 app = Flask(__name__)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Récupérez le paramètre entré dans le formulaire
         params = request.form.get('parametre')
-
+        name=params[1:].replace("/","_")
         # Utilisez la fonction getJson pour obtenir le DataFrame
         df = getJson(params)
+
+        # Appel de la fonction post_processing pour générer l'image
+        post_processing(name,df)
 
         # Transformez le DataFrame en HTML pour l'afficher dans la page
         df_html = df.to_html(classes='table table-bordered', index=False)
 
-        return render_template('index.html', dataframe=df_html)
+        # Retournez le modèle HTML en passant le nom du fichier image
+        return render_template('index.html', dataframe=df_html, image_path="" + name+".png")
 
     return render_template('index.html')
